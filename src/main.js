c36773ff3de010a8f469617513da8a782e1c1b9e
jihad mia <jihadmiaweb@gmail.com>
2025-09-12 23:01:27 +0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// script.js
+
+var typed = new Typed('.text', {
+    strings: [
+
+        'Frontend Developer',
+        'Web Developer',
+
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
